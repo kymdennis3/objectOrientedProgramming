cdd633795c96e4acc05389985603648bf6b4e07b
kymdennis3 <kymdennis3@gmail.com>
2022-11-16 06:36:55 +0300

    Created an instance of another class and printed out one of car's attributes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        car myCar = new car(); // constructing an instance of another class
+        //object = an instance of a class that may contain attributes and methods
+        // example: (phone, desk, computer,coffee cup)
+
+        System.out.println(myCar.color);
     }
 }
---
+++ src/car.java
@@ -0,0 +1,14 @@
+public class car {
+    String make = " Ford";
+    String model = " Ford";
+    int year = 2021;
+    String color = "Brown";
+    double price = 4500000.00;
+
+    void drive(){
+        System.out.println("You drive the car");
+    }
+    void brake(){
+        System.out.println("You stopped the car");
+    }
+}
