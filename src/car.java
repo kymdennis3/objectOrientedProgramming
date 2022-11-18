0b7b32d21027604e5707ab3c90ef531f0409a89c
kymdennis3 <kymdennis3@gmail.com>
2022-11-18 20:32:22 +0300

    created a second object and displayed both of their attributes

--- src/car.java
+++ src/car.java
@@ -1,12 +1,12 @@
 public class car {
-    String make = "Ford";
-    String model = "Ford";
+    String make = "Toyota";
+    String model = "ZX";
     int year = 2021;
     String color = "Brown";
     double price = 4500000.00;
 
     void drive(){
-        System.out.println("You drive the car");
+        System.out.println("You are driving the car");
     }
     void brake(){
         System.out.println("You stopped the car");
